960a4399fd3755bd04dd6a32dda18739451cdc53
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-02-12 14:50:36 -0300

    feat/build

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  reactStrictMode: true,
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
 
